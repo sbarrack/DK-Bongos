4db18e373bf3e43fd230626801de1ec549a3bc72
sbarrack <12619603+sbarrack@users.noreply.github.com>
2018-10-24 22:43:41 -0700

    before adding back guitar code
    :musical_keyboard: :hand: :hand: :sunglasses: :tophat:

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,5 +1,5 @@
-/* Borrowed this from some post I found online that was really helpful and gave me a lot of ideas.
- * Link: https://forum.pjrc.com/threads/26753-Reading-an-N64-controller-getting-no-data
+/* Shout out to Rena, whoever you are, your code was really helpful!
+ * https://forum.pjrc.com/threads/26753-Reading-an-N64-controller-getting-no-data
  */
 
 /* timings http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0344k/Cfacfihf.html
@@ -11,37 +11,41 @@
  * nop = 1, none if not volatile
  */
 
+#include <Arduino.h>
+#include "types.h"
+#include "revolution.h"
+
 union N64Report {
-	uint8_t raw[4];
+	u8 raw[4];
 	//uint16_t raw16[2];
-	uint32_t raw32;
+	u32 raw32;
 	// TODO fix reversed bytes?
 	struct {
-		int8_t sy;
-		int8_t sx;
+		s8 sy;
+		s8 sx;
 
-		uint8_t cr : 1;
-		uint8_t cl : 1;
-		uint8_t cd : 1;
-		uint8_t cu : 1;
-		uint8_t r : 1;
-		uint8_t l : 1;
-		uint8_t reset : 1;
-		uint8_t : 1;
+		u8 cr : 1;
+		u8 cl : 1;
+		u8 cd : 1;
+		u8 cu : 1;
+		u8 r : 1;
+		u8 l : 1;
+		u8 reset : 1;
+		u8 : 1;
 		
-		uint8_t dr : 1;
-		uint8_t dl : 1;
-		uint8_t dd : 1;
-		uint8_t du : 1;
-		uint8_t start : 1;
-		uint8_t z : 1;
-		uint8_t b : 1;
-		uint8_t a : 1;
+		u8 dr : 1;
+		u8 dl : 1;
+		u8 dd : 1;
+		u8 du : 1;
+		u8 start : 1;
+		u8 z : 1;
+		u8 b : 1;
+		u8 a : 1;
 	};
 } n64data, olddata;
 
-// Copied from avr_emulation.h; computes the address of the mode register for the given pin.
-#define GPIO_BITBAND_ADDR(reg, bit) (((uint32_t)&(reg) - 0x40000000) * 32 + (bit) * 4 + 0x42000000)
+// address for GPIO mode register of given pin
+#define GPIO_BITBAND_ADDR(reg, bitt) (((u32)&(reg) - 0x40000000) * 32 + (bitt) * 4 + 0x42000000)
 
 #define SET_LOW "str %4, [%2, 4]\n" // set the outport low
 #define SET_HIGH "str %4, [%2, 0]\n" // set the outport high
@@ -62,11 +66,11 @@ union N64Report {
 
 #define PIN 0
 
-static inline uint32_t poll() {
-	static uint32_t modeReg = GPIO_BITBAND_ADDR(CORE_PIN0_PORTREG, CORE_PIN0_BIT);
-	uint32_t buffer[33]; // 32 bits; 1 stop bit?
+static inline u32 poll() {
+	static u32 modeReg = GPIO_BITBAND_ADDR(CORE_PIN0_PORTREG, CORE_PIN0_BIT);
+	u32 buffer[33]; // 32 bits; 1 stop bit?
 
-	uint8_t oldSREG = SREG;
+	u8 oldSREG = SREG;
 	cli();
 	__asm__ volatile (
 		"ldr r0,=0\n"
@@ -96,7 +100,7 @@ static inline uint32_t poll() {
 	SREG = oldSREG;
 
 	// parse
-	uint32_t data = 0;
+	u32 data = 0;
 	for (int i = 0; i < 32; i++) {
 		if (buffer[i] & CORE_PIN0_BITMASK) {
 			data |= 1 << (31 - i);
@@ -117,7 +121,8 @@ int umod;
 void loop() {
 	n64data.raw32 = poll();
 
-	// TODO have input buffer for mod buttons only, L+R = extra harmony/dual chambers, individual sharps and flats?
+	// TODO input buffer for mod buttons?;
+	// L, R = apply mod to 2nd, 3rd note pressed only? fixed harmony, toggle major/minor/interval?
 	int mod = n64data.du - n64data.dd + (n64data.dr ? 12 : 0) - (n64data.dl ? 12 : 0);
 	if (umod != mod) {
 		usbMIDI.sendNoteOff(62 + umod, 63, 1);
@@ -169,5 +174,5 @@ void loop() {
 	umod = mod;
 	olddata = n64data;
 
-	delay(5); // more error the shorter the time between polls
+	delay(10); // more error the shorter the time between polls, less time to buffer inputs too
 }
