e4f35ef6087271bf87a00c83d4f1b928219209b5
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-06 08:17:22 -0700

    Update Project-Bongo.ino
    works with old nano bootloader

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,10 +1,20 @@
+#include <Nintendo.h>
+
+CGamecubeController gcc(7);
+CGamecubeConsole gc(8);
 
 void setup()
 {
-
+    pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+    gcc.read();
+    auto rep = gcc.getReport();
+
+    rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
+    rep.left = 0x20;
     
+    gc.write(rep);
 }
