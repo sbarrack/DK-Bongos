b8c04b9ed7505b3140b4fef03c1607a50c32fadb
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-07 12:43:45 -0700

    Update Project-Bongo.ino

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -3,9 +3,11 @@
 CGamecubeController gcc(7);
 CGamecubeConsole gc(8);
 
+void yield() {}
+
 void setup()
 {
-    pinMode(13, OUTPUT);
+
 }
 
 void loop()
@@ -15,6 +17,6 @@ void loop()
 
     rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
     rep.left = 0x20;
-    
+
     gc.write(rep);
 }
