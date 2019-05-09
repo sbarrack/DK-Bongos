ac2e242a279b30db7fe4ec0c2189c0dc8ce57522
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-09 08:28:04 -0700

    Update Project-Bongo.ino
    use timer to determine when we need to poll the controller (120Hz = ~every 8333us), technically could be anything but this is already the standard and a good balance between debounce and accuracy
    needed for consistancy later on when I record inputs
    timer is non-blocking

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -2,19 +2,29 @@
 
 CGamecubeController gcc(7);
 CGamecubeConsole gc(8);
+Gamecube_Report_t rep;
+unsigned long timeNow, timeMark, timeMarkOld;
 
 void setup()
 {
-
+    rep = defaultGamecubeData.report;
 }
 
 void loop()
 {
     for (;;) {
-        gcc.read();
-        auto rep = gcc.getReport();
-        rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
-        rep.left = 0x49;
+        timeNow = micros();
+
+        // if (dTime >= .5 frames || overflow < -.5 frames)
+        if (timeNow >= timeMark || timeNow < timeMarkOld) {
+            timeMarkOld = timeMark;
+            timeMark += 8333;
+
+            gcc.read();
+            rep = gcc.getReport();
+            rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
+        }
+
         gc.write(rep);
     }
 }
