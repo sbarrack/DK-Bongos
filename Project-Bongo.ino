a85606861628b42efa517be343e516ed4544b2d7
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-08 22:13:34 -0700

    Update Project-Bongo.ino
    main() is different on AVR, the indefinite loop is better anyway for readability

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -3,8 +3,6 @@
 CGamecubeController gcc(7);
 CGamecubeConsole gc(8);
 
-// void yield() {}
-
 void setup()
 {
 
@@ -12,11 +10,11 @@ void setup()
 
 void loop()
 {
-    gcc.read();
-    auto rep = gcc.getReport();
-
-    rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
-    rep.left = 0x20;
-
-    gc.write(rep);
+    for (;;) {
+        gcc.read();
+        auto rep = gcc.getReport();
+        rep.xAxis = rep.yAxis = rep.cxAxis = rep.cyAxis = 0x80;
+        rep.left = 0x49;
+        gc.write(rep);
+    }
 }
