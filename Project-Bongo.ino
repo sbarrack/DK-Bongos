197911556ea7410cbdb638a3150c14d8ab4b93eb
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-17 22:01:21 -0700

    Update Project-Bongo.ino
    working SD card
    too slow for real time, can still definately use it for Gameboy but need a different approach for bongos

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,20 +1,11 @@
-// #include <SPI.h>
 #include <SD.h>
 #include <GamecubeAPI.h>
 
-// gcc.read();
-// buffer[1] = released = buffer[0];
-// buffer[0] = pressed = held = gcc.getReport();
-// gc.write(buffer[0]);
-// uint16_t tmp = buffer[0].raw16[0] ^ buffer[1].raw16[0];
-// pressed.raw16[0] &= tmp;
-// held.raw16[0] = (buffer[0].raw16[0] & buffer[1].raw16[0]);
-// released.raw16[0] &= tmp;
+#define TEST_FILE "test.tas"
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial);
 
     while (initSD());
@@ -38,176 +29,81 @@ void loop()
 
 CGamecubeController gcc(7);
 CGamecubeConsole gc(8);
-Gamecube_Report_t buffer[2], pressed, held, released;
+Gamecube_Report_t buffer[2], pressed, held;
+
+void updateBuffers() {
+    gcc.read();
+    buffer[1] = buffer[0];
+    buffer[0] = pressed = held = gcc.getReport();
+    pressed.raw16[0] &= buffer[0].raw16[0] ^ buffer[1].raw16[0];
+    held.raw16[0] = (buffer[0].raw16[0] & buffer[1].raw16[0]);
+    gc.write(buffer[0]);
+}
 
 void doNormal() {
     while (mode == normal) {
-        gcc.read();
-        buffer[1] = buffer[0];
-        buffer[0] = pressed = gcc.getReport();
-        gc.write(buffer[0]);
-        pressed.raw16[0] &= buffer[0].raw16[0] ^ buffer[1].raw16[0];
+        updateBuffers();
 
-        if (pressed.ddown) {
+        if (pressed.dleft) {
             mode = record;
-        } else if (false) { // TODO
+        } else if (pressed.dright) {
             mode = playback;
         }
     }
 }
 
-union TASHeader {
-    uint8_t raw[256];
-    struct {
-        uint32_t signature = "DTM\x1a";
-        uint8_t gameID[6] = "GALE01";
-        uint8_t isWiiGame; // bool
-
-        uint8_t gcc1 : 1;
-        uint8_t gcc2 : 1;
-        uint8_t gcc3 : 1;
-        uint8_t gcc4 : 1;
-        uint8_t wiimote1 : 1;
-        uint8_t wiimote2 : 1;
-        uint8_t wiimote3 : 1;
-        uint8_t wiimote4 : 1;
-
-        uint8_t fromSaveState; // bool
-        uint64_t VIcount; // int
-        uint64_t inputCount; // int
-        uint64_t lagCounter; // int
-        uint64_t; // reserved
-        uint32_t rerecordCount; // int
-        uint8_t author[32] = "Stephen \"Bongo Boy\" Barrack";
-        uint8_t videoBackend[16]; // string
-        uint8_t audioEmulator[16]; // int
-        uint8_t checksum[16]; // raw, MD5 of the game
-        uint64_t startTimeUNIX; // int
-        uint8_t validConfig; // bool
-        uint8_t idleSkipping; // bool
-        uint8_t dualCore; // bool
-        uint8_t progressiveScan; // bool
-        uint8_t HLEenabled; // bool
-        uint8_t fastDisc; // bool
-        uint8_t CPUcore; // int: 0 interpret, 1 JIT, 2 JITIL
-        uint8_t EFBaccess; // bool
-        uint8_t EFBCopy; // bool
-        uint8_t EFBtoTexture; // bool
-        uint8_t EFBCache; // bool
-        uint8_t formatChanges; // bool
-        uint8_t useXFB; // bool
-        uint8_t realXFB; // bool
-        
-        uint8_t memCardA : 1;
-        uint8_t memCardB : 1;
-        uint8_t : 6;
-
-        uint8_t memCardBlank; // bool
-
-        uint8_t bongo1 : 1;
-        uint8_t bongo2 : 1;
-        uint8_t bongo3 : 1;
-        uint8_t bongo4 : 1;
-        uint8_t : 4;
-
-        uint8_t syncGPU; // bool
-        uint8_t netplay; // bool
-        uint8_t PAL60; // bool
-        uint8_t[12]; // reserved
-        uint8_t discTwoISO[40]; // string
-        uint8_t hash[20]; // raw, SHA-1 of git rev
-        uint32_t IROMhash; // int
-        uint32_t COEFhash; // int
-        uint64_t tickCount; // int, 486MHz GC, 729MHz Wii
-        uint8_t[11]; // reserved
-    };
-} header;
-
 File tas;
 
-union TASReport {
-    uint8_t raw8[8];
-    uint16_t raw16[0];
-    uint32_t raw32[0];
-
-    struct {
-        uint8_t start : 1;
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t z : 1;
-        uint8_t du : 1;
-        uint8_t dd : 1;
-
-        uint8_t dl : 1;
-        uint8_t dr : 1;
-        uint8_t l : 1;
-        uint8_t r : 1;
-        uint8_t disc : 1;
-        uint8_t reset : 1;
-        uint8_t connected : 1;
-        uint8_t : 1;
-
-        uint8_t lt, rt, sx, sy, cx, cy;
-    };
-};
-
-uint8_t * toTAS(Gamecube_Report_t rep) {
-    union TASReport tmp;
-    
-    tmp.start = rep.start;
-    tmp.a = rep.a;
-    tmp.b = rep.b;
-    tmp.x = rep.x;
-    tmp.y = rep.y;
-    tmp.z = rep.z;
-    tmp.du = rep.dup;
-    tmp.dd = rep.ddown;
-
-    tmp.dl = rep.dleft;
-    tmp.dr = rep.dright;
-    tmp.l = rep.l;
-    tmp.r = rep.r;
-    tmp.connected = 1;
-    
-    tmp.raw16[1] = rep.raw16[3]; // lt, rt
-    tmp.raw16[2] = rep.raw16[1]; // sx, sy
-    tmp.raw16[3] = rep.raw16[2]; // cx, cy
-
-    return tmp.raw8;
-}
-
 void doRecord() {
-    Serial.print("Recording... ");
-    tas = SD.open("thing.dtm"/* TODO generate name */, FILE_WRITE);
+    // TODO unique filenames
+    if (SD.exists(TEST_FILE)) {
+        // Serial.print("Removing old file... ");
+        if (!SD.remove(TEST_FILE)) {
+            // Serial.println("FAILED");
+            mode = normal;
+            return;
+        }
+        // Serial.println("DONE");
+    }
+
+    // Serial.print("Recording... ");
+    tas = SD.open(TEST_FILE, FILE_WRITE); // TODO generate new name and save it
     if (!tas) {
-        Serial.println("FAILED");
+        // Serial.println("FAILED");
         mode = normal;
         return;
     }
-    // init header before the whole shabang
-    tas.write(header.raw, 256);
-
+    
     while (mode == record) {
-        gcc.read();
-        buffer[1] = buffer[0];
-        buffer[0] = pressed = gcc.getReport();
-        gc.write(buffer[0]);
+        updateBuffers();
 
-        tas.write(toTAS(buffer[0]), 8);
+        tas.write(buffer[0].raw8, 8);
 
-        if (pressed.ddown) {
+        if (pressed.dleft) {
             mode = normal;
         }
     }
 
-    Serial.println("DONE");
+    // Serial.println("DONE");
     tas.close();
 }
 
 void doPlayback() {
-    // TODO
+    // Serial.print("Replaying... ");
+    tas = SD.open(TEST_FILE); // TODO get last TAS file
+    mode = normal; // either way
+    if (!tas) {
+        // Serial.println("FAILED");
+        return;
+    }
+
+    while (tas.available()) {
+        tas.readBytes(held.raw8, 8);
+        gc.write(held);
+    }
+
+    // Serial.println("DONE");
+    tas.close();
 }
 
 bool initSD() {
@@ -221,7 +117,7 @@ bool initSD() {
     } else {
         // SD card is not inserted or faulty wiring
         Serial.println("MISSING");
-        return false;
+        return true;
     }
 
     Serial.print("Type: ");
@@ -234,17 +130,18 @@ bool initSD() {
             Serial.println("SDHC");
             break;
         default:
-            // No SDXC cards
+            // No SDXC cards (>32GB)
             Serial.println("INVALID");
-            return false;
+            return true;
     }
 
     Serial.print("Checking partition... ");
     if (!part.init(card)) {
         // Format to FAT16 or FAT32
         Serial.print("INVALID");
-        return false;
+        return true;
     }
+    Serial.println("GOOD");
 
     Serial.print("Volume: FAT");
     Serial.print(part.fatType(), DEC);
@@ -259,9 +156,9 @@ bool initSD() {
     Serial.print("Loading card... ");
     if (!SD.begin()) {
         Serial.println("FAILED");
-        return false;
+        return true;
     }
     Serial.println("DONE");
 
-    return true;
+    return false;
 }
