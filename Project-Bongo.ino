7c6388f21e51623d7afc97229fab3efcc2feac20
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 15:55:33 -0700

    still...

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,5 +1,7 @@
 #include <GamecubeAPI.h>
 
+#define BUFF_SIZE 8
+
 // 2-9,23-26
 CGamecubeConsole gc(2);
 CGamecubeController gcc(3); // have more controllers
@@ -8,21 +10,21 @@ Gamecube_Report_t out, in = defaultGamecubeData.report;
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(115200); // SERIAL_8N1
     // Serial.setTimeout(1000);
-    while (!Serial);
+    // while (!Serial);
 }
 
 void loop()
 {
-    gcc.read();
-    out = gcc.getReport();
-    if (Serial.availableForWrite() >= (int)sizeof(out.raw8)) {
-        Serial.write(out.raw8, sizeof(out.raw8));
+    if (Serial.availableForWrite() >= BUFF_SIZE) {
+        gcc.read();
+        out = gcc.getReport();
+        Serial.write(out.raw8, BUFF_SIZE);
         Serial.flush();
     }
-    if (Serial.available() >= (int)sizeof(in.raw8)) {
-        Serial.readBytes(in.raw8, sizeof(in.raw8));
+    if (Serial.available() >= BUFF_SIZE) {
+        Serial.readBytes(in.raw8, BUFF_SIZE);
     }
     gc.write(in);
 }
