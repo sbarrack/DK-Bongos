e0addd8749adebde9fbead6ba747f9f9a3a48746
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 15:50:48 -0700

    still doing it :P

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -2,7 +2,7 @@
 uint8_t buffer[8];
 
 void setup() {
-    Serial1.begin(115200);
+    Serial1.begin(115200, SERIAL_8N1);
     // Serial1.setTimeout(1000);
     while (!Serial1);
 }
@@ -10,9 +10,9 @@ void setup() {
 void loop() {
     if (Serial1.available() >= (int)sizeof(buffer)) {
         Serial1.readBytes(buffer, sizeof(buffer));
-        if (Serial1.availableForWrite() >= (int)sizeof(buffer)) {
-            Serial1.write(buffer, sizeof(buffer));
-            Serial1.flush();
-        }
+    }
+    if (Serial1.availableForWrite() >= (int)sizeof(buffer)) {
+        Serial1.write(buffer, sizeof(buffer));
+        Serial1.flush();
     }
 }
