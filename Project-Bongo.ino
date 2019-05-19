11347ed0923e5bd6de81f7732ccbfbf89c6ea304
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 15:35:34 -0700

    still crossing my fingers

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -3,12 +3,16 @@ uint8_t buffer[8];
 
 void setup() {
     Serial1.begin(115200);
+    // Serial1.setTimeout(1000);
+    while (!Serial1);
 }
 
 void loop() {
     if (Serial1.available() >= (int)sizeof(buffer)) {
         Serial1.readBytes(buffer, sizeof(buffer));
-        buffer[0] = (buffer[0] & 0xfe) | ((~buffer[0]) & 1); // ~a
-        Serial1.write(buffer, sizeof(buffer));
+        if (Serial1.availableForWrite() >= (int)sizeof(buffer)) {
+            Serial1.write(buffer, sizeof(buffer));
+            Serial1.flush();
+        }
     }
 }
