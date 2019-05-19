d52b8de5b11a47406b97792b1ee3657d26ad545e
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 15:44:48 -0700

    argh

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -4,16 +4,23 @@
 CGamecubeConsole gc(2);
 CGamecubeController gcc(3); // have more controllers
 
+Gamecube_Report_t out, in = defaultGamecubeData.report;
+
 void setup()
 {
     Serial.begin(115200);
+    // Serial.setTimeout(1000);
+    while (!Serial);
 }
 
 void loop()
 {
     gcc.read();
-    auto in = gcc.getReport();
-    Serial.write(in.raw8, sizeof(in.raw8));
+    out = gcc.getReport();
+    if (Serial.availableForWrite() >= (int)sizeof(out.raw8)) {
+        Serial.write(out.raw8, sizeof(out.raw8));
+        Serial.flush();
+    }
     if (Serial.available() >= (int)sizeof(in.raw8)) {
         Serial.readBytes(in.raw8, sizeof(in.raw8));
     }
