adf2c4fe7a028979d5dea001a078b2c8d6acd753
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 14:48:36 -0700

    Update Project-Bongo.ino
    does serial work?

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,8 +1,14 @@
 
-void setup() {
+uint8_t buffer[8];
 
+void setup() {
+    Serial1.begin(115200);
 }
 
 void loop() {
-
+    if (Serial1.available() >= (int)sizeof(buffer)) {
+        Serial1.readBytes(buffer, sizeof(buffer));
+        buffer[0] = (buffer[0] & 0xfe) | ((~buffer[0]) & 1); // ~a
+        Serial1.write(buffer, sizeof(buffer));
+    }
 }
