8553fb12f7cb498d9e62cb2dc5aaffefc529a986
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 15:00:01 -0700

    Update Project-Bongo.ino
    wonder if it works

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -4,12 +4,20 @@
 CGamecubeConsole gc(2);
 CGamecubeController gcc(3); // have more controllers
 
+Gamecube_Report_t out = defaultGamecubeData.report;
+
 void setup()
 {
-
+    Serial.begin(115200);
 }
 
 void loop()
 {
-
+    gcc.read();
+    auto in = gcc.getReport();
+    Serial.write(in.raw8, sizeof(in.raw8));
+    if (Serial.available() >= (int)sizeof(in.raw8)) {
+        Serial.readBytes(out.raw8, sizeof(out.raw8));
+    }
+    gc.write(out);
 }
