0c38ff7f01f2efbad837a08e2a32b76f51086760
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 18:55:53 -0700

    switched master w/slave

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -1,35 +1,35 @@
 #include <Wire.h>
 #include <GamecubeAPI.h>
 
+#define I2C_ADDR 8
+
 CGamecubeConsole gc(2);
 CGamecubeController gcc(3); // have more controllers
-
 Gamecube_Report_t out, in = defaultGamecubeData.report;
+
+int timeout;
 
 void setup()
 {
     Wire.setClock(400000); // fast mode
-    Wire.begin(8); // slave
-
-    Wire.onRequest(send);
-    Wire.onReceive(get);
+    Wire.begin(); // master
 }
 
 void loop()
-{
+{    
     gcc.read();
     out = gcc.getReport();
-    gc.write(in);
-}
 
-void send() {
+    // send
+    Wire.beginTransmission(I2C_ADDR);
     Wire.write(out.raw8, sizeof(out.raw8));
-}
+    Wire.endTransmission();
 
-void get() {
-    if (Wire.available() >= (long long)sizeof(in.raw8)) {
-        for (int i = 0; i < sizeof(in.raw8); i++) {
-            in.raw8[i] = Wire.read();
-        }
+    // get
+    Wire.requestFrom(I2C_ADDR, sizeof(in.raw8));
+    for (int i = 0; Wire.available() && i < sizeof(in.raw8); i++) {
+        in.raw8[i] = Wire.read();
     }
+
+    gc.write(in);
 }
