c27354076af1a957143f470e9e00a170bad0b851
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-19 19:55:16 -0700

    IT WORKS! LE BONGOS ARE LEGIT AF!

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -2,26 +2,95 @@
 
 #define I2C_ADDR 8
 
-uint8_t buffer[8];
+union {
+    uint8_t raw[8];
+    uint16_t raw16[4];
+    uint32_t raw32[2];
 
-void setup() {
+    struct
+    {
+        union {
+            uint16_t buttons;
+            struct
+            {
+                uint8_t a : 1;
+                uint8_t b : 1;
+                uint8_t x : 1;
+                uint8_t y : 1;
+                uint8_t start : 1;
+                uint8_t : 3;
+
+                union {
+                    uint8_t dpad : 4;
+                    struct
+                    {
+                        uint8_t dl : 1;
+                        uint8_t dr : 1;
+                        uint8_t dd : 1;
+                        uint8_t du : 1;
+                    };
+                };
+                uint8_t z : 1;
+                uint8_t r : 1;
+                uint8_t l : 1;
+                uint8_t : 1;
+            };
+        };
+
+        uint8_t sx, sy, cx, cy, lt, rt;
+    };
+} rep;
+
+union {
+    uint8_t raw[2];
+    uint16_t raw16;
+
+    struct
+    {
+        union {
+            uint8_t buttons;
+            struct
+            {
+                // bongo w/ start facing in
+                uint8_t br : 1;
+                uint8_t bl : 1;
+                uint8_t tr : 1;
+                uint8_t tl : 1;
+                uint8_t start : 1;
+                uint8_t : 3;
+            };
+        };
+
+        uint8_t mic;
+    };
+} bongo;
+
+void setup()
+{
     Wire.setClock(400000); // fast mode
-    Wire.begin(I2C_ADDR); // slave
+    Wire.begin(I2C_ADDR);  // slave
 
     Wire.onReceive(get);
     Wire.onRequest(send);
 }
 
-void loop() {
-
+void loop()
+{
+    // normal bongos
+    rep.raw[0] = bongo.buttons;
+    rep.raw[8] = bongo.mic;
+    rep.raw16[1] = rep.raw16[2] = 0x8080;
 }
 
-void get(int c) {
-    for (int i = 0; i < c; i++) {
-        buffer[i] = Wire.read();
+void get(int c)
+{
+    for (int i = 0; i < c; i++)
+    {
+        bongo.raw[i] = Wire.read();
     }
 }
 
-void send() {
-    Wire.write(buffer, sizeof(buffer));
+void send()
+{
+    Wire.write(rep.raw, sizeof(rep.raw));
 }
