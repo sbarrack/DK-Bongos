16646a5510979c1f815f8f9fd2129e69c51691b9
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-05-21 18:35:51 -0700

    Update Project-Bongo.ino
    succesful id-ing

--- Project-Bongo.ino
+++ Project-Bongo.ino
@@ -73,34 +73,115 @@ union {
     };
 } bongo;
 
-uint8_t wiiExtID[6];
+// bytes are backwards cause unions are little endian in c and we don't got time for that!
+enum WiiExtFull : uint64_t {
+    Nunchuck = 0x000020a40000,
+    Guitar = 0x030120a40000,
+    Drums = 0x030120a40001
+
+    // don't care about classic, tablet, or tatacon
+    // turntable soon(tm)
+    // balance board is not accessible
+    // wii motion+ is stupid so no
+};
 
 union {
     uint8_t raw[6];
+    WiiExtFull type;
+} wiiExtID;
 
+// buttons/booleans are inverted, fyi
+// alternative is to bitwise NOT all incoming data
+union {
+    uint8_t raw[6];
+
+    // http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Nunchuck
+    // Don't expect me to explain these for ya
     struct
     {
         uint8_t sx;
         uint8_t sy;
 
-        // missing least 2 sig bits
-        uint8_t ax;
-        uint8_t ay;
-        uint8_t az;
+        uint16_t ax : 8;
+        uint16_t ay : 8;
+        uint16_t az : 8;
 
-        // buttons on wii ext are NOT'd, treat them as booleans
         uint8_t z : 1;
         uint8_t c : 1;
-        // the extra bits
-        uint8_t ax1 : 2;
-        uint8_t ay1 : 2;
-        uint8_t az1 : 2;
+        uint16_t ax1 : 2;
+        uint16_t ay1 : 2;
+        uint16_t az1 : 2;
     } nunchuck;
+
+    // http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Guitar_Hero_(Wii)_Guitars
+    struct {
+        uint8_t sx : 6;
+        uint8_t : 1;
+        uint8_t guitarType : 1;
+
+        uint8_t sy : 6;
+        uint8_t : 2;
+
+        uint8_t fretbar : 5; // funky values
+        uint8_t : 3;
+
+        uint8_t whammybar : 5;
+        uint8_t : 3;
+
+        uint8_t : 2;
+        uint8_t start : 1; // either/both +'s
+        uint8_t : 1;
+        uint8_t select : 1; // - or starpower
+        uint8_t : 1;
+        uint8_t dd; // strum down
+        uint8_t : 1;
+
+        uint8_t du; // strum up
+        uint8_t : 2;
+        // these have normal button labels, check mobo inside
+        uint8_t yellow : 1;
+        uint8_t green : 1;
+        uint8_t blue : 1;
+        uint8_t red : 1;
+        uint8_t orange : 1;
+    } guitar;
+
+    // http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Guitar_Hero_World_Tour_(Wii)_Drums
+    struct {
+        uint8_t sx : 6;
+        uint8_t : 2;
+
+        uint8_t sy : 6;
+        uint8_t : 2;
+
+        uint8_t : 1;
+        uint8_t vSelect : 5; // which pad the velocity is for
+        uint8_t vNone : 1; // if there's no velocity
+        uint8_t l : 1; // high-hat pedal
+
+        uint8_t : 5;
+        uint8_t velocity : 3; // low values = more velocity, high-hat and bass share one
+
+        uint8_t : 2;
+        uint8_t start : 1; // +
+        uint8_t : 1;
+        uint8_t select : 1; // -
+        uint8_t : 3;
+
+        uint8_t : 2;
+        uint8_t r : 1; // bass pedal
+        uint8_t blue : 1;
+        uint8_t green : 1;
+        uint8_t yellow : 1;
+        uint8_t red : 1;
+        uint8_t orange : 1;
+    } drums;
 } wiiExtRep;
 
 void setup()
 {
     Serial.begin(115200);
+    // while (!Serial);
 
     // nano
     Wire.setClock(400000); // fast mode
@@ -113,15 +194,15 @@ void setup()
     Wire1.begin(); // master
     // init
     Wire1.beginTransmission(WII_EXT_ADDR);
-    Wire1.write(0xf0); // register 0xA400F0
-    Wire1.write(0x55); // data
+    Wire1.write(0xf0); // @ 0x(4)a400f0
+    Wire1.write(0x55); // write 0x55
     while (Wire1.endTransmission());
     delayMicroseconds(38); // min delay
     Wire1.beginTransmission(WII_EXT_ADDR);
     Wire1.write(0xfb);
     Wire1.write(0);
     while (Wire1.endTransmission());
-    // idWiiExt();
+    idWiiExt();
 
     SDerrors.init = !sd.begin(SdioConfig(FIFO_SDIO));
     // testFileSD();
@@ -132,7 +213,8 @@ void loop()
     // poll wii ext
     delayMicroseconds(42);
     Wire1.beginTransmission(WII_EXT_ADDR);
-    Wire1.write(0);
+    Wire1.write(0); // @ addr 0x(4)a40000
+                    // read
     while (Wire1.endTransmission());
     delayMicroseconds(149);
     int n = Wire1.requestFrom(WII_EXT_ADDR, sizeof(wiiExtRep.raw));
@@ -142,10 +224,12 @@ void loop()
     }
 
     // wii ext
-    rep.sx = wiiExtRep.nunchuck.sx;
-    rep.sy = wiiExtRep.nunchuck.sy;
-    rep.z = !wiiExtRep.nunchuck.z;
-    rep.du = !wiiExtRep.nunchuck.c;
+    if (wiiExtID.type == Nunchuck) {
+        rep.sx = wiiExtRep.nunchuck.sx;
+        rep.sy = wiiExtRep.nunchuck.sy;
+        rep.z = !wiiExtRep.nunchuck.z;
+        rep.du = !wiiExtRep.nunchuck.c;
+    } // TODO add other cases
     // normal bongos
     rep.raw[0] = bongo.buttons;
     rep.raw[7] = bongo.mic;
@@ -157,7 +241,7 @@ void loop()
     // }
 }
 
-void yield() {} // get rid of the most useless function ever
+void yield() {}
 
 void get(int c)
 {
@@ -180,10 +264,10 @@ void idWiiExt()
     Wire1.write(0);
     while (Wire1.endTransmission());
     delayMicroseconds(38);
-    int n = Wire1.requestFrom(WII_EXT_ADDR, sizeof(wiiExtID));
+    int n = Wire1.requestFrom(WII_EXT_ADDR, sizeof(wiiExtID.raw));
     for (int i = 0; i < n; i++)
     {
-        wiiExtID[i] = Wire1.read();
+        wiiExtID.raw[i] = Wire1.read();
     }
 }
 
