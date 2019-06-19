d2e00c25236be0b69644e241db07509211747695
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-06-19 15:17:53 -0700

    more fix, needs test
    solder the 4 pins linked in 16u2.ino and test the other sketch to see how well the Nintendo library actually works

--- 16u2/16u2.ino
+++ 16u2/16u2.ino
@@ -1,3 +1,5 @@
+// led logic is inverted, pins are different than 328p!
+// https://github.com/NicoHood/HoodLoader2/wiki/4-Pin-Header
 
 void setup()
 {
--- 328p/328p.ino
+++ 328p/328p.ino
@@ -1,27 +1,48 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+#include <GamecubeAPI.h>
+// #include <HID-Project.h>
 
-  This example code is in the public domain.
- */
+CGamecubeController bongos(7);
+// CGamecubeConsole console(8);
 
-// Pin 13 has an LED connected on most Arduino boards.
-// Pin 11 has the LED on Teensy 2.0
-// Pin 6  has the LED on Teensy++ 2.0
-// Pin 13 has the LED on Teensy 3.0
-// give it a name:
-int led = 13;
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    while (!Serial);
+    Serial.begin(115200);
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
+    // Gamepad.begin();
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void loop()
+{
+    if (bongos.read()) {
+        auto status = bongos.getStatus();
+        auto report = bongos.getReport();
+
+        Serial.print(F("HIT "));
+        Serial.print(status.device, HEX);
+        Serial.print(F(" | "));
+        Serial.print(report.buttons0, BIN);
+        Serial.print(F(" "));
+        Serial.print(report.right, HEX);
+
+        // for Windows calibration comment out the l/r buttons below
+        // (because l/r triggers the wizard)
+        // Gamepad.rzAxis(report.right - 0x80);
+        // Gamepad.buttons(0x00UL | (report.buttons0 & 0x1F) | ((report.buttons1 & 0x70) << 1));
+        // Gamepad.write();
+
+        // if (console.write(report)) {
+        //     Serial.println(F(" HIT"));
+        //     digitalWrite(LED_BUILTIN, LOW);
+        // } else {
+        //     Serial.println(F(" MISS"));
+        //     digitalWrite(LED_BUILTIN, HIGH);
+        // }
+    } else {
+        Serial.println(F("MISS"));
+        digitalWrite(LED_BUILTIN, HIGH);
+        
+        // Gamepad.end();
+    }
 }
