aa64619003a0d2da65531dcfa40c396a05c998e2
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-07-24 10:49:44 -0700

    forgot something

--- DK-Bongos.ino
+++ DK-Bongos.ino
@@ -6,7 +6,6 @@
 // use O_ flags and not FILE_ flags
 // use sdformatter.exe
 
-#define STOP_BYTE 0xff
 Gamecube_Report_t report = defaultGamecubeData.report;
 CGamecubeController controller(2);
 CGamecubeConsole console(3);
@@ -16,7 +15,6 @@ void setup()
     // Serial.begin(2000000);
 
     // use "continue;" instead of break, use break to exit
-    // eventually, switch to a makefile instead of IDE
     for (;;)
     {
         // TODO minify nintendo library in this project
